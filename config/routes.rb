a86f5bc158b285ef2148af58c40833dba377bb49
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-03-16 15:16:14 +0000

    20230317.00:16

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 get 'homes/about' => 'homes#about' , as:"about"
+get 'books/:id' => 'books#show' ,as:"book"
+
+
+resource :books, only:[:new, :index, :show]
+
 end
