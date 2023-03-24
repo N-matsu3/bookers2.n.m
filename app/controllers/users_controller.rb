08910e027e3f179c147ffa747138e30109a0633d
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-03-24 07:08:49 +0000

    20230324.16:08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+
+  end
+
   def new
     @book = Book.new
   end
@@ -19,20 +26,41 @@ class UsersController < ApplicationController
         flash[:notice] = "You have created book successfully."
       redirect_to '/books#show'
      else
-      render :books
+      render :'/users/:id'
      end
-
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @book = Book.new
-    @books = @user.books
 
   end
 
   def edit
     @user = User.find(params[:id])
+    @book = Book.find(params[:id])
+
   end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully created."
+
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 
 end
