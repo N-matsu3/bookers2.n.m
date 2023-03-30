5be4caa457048400f28f99b9fa9c9b7e40c2dbbb
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-03-30 17:10:46 +0000

    20230331/2:10

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 
-get 'homes/about' => 'homes#about'
-
+get 'home/about' => 'homes#about'
 get 'users/:id' => 'users#show'
 get 'users/edit'
 
